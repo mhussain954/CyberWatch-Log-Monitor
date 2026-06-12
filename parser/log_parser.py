ca75894f8828d2dd9eef847eb1048d4f8fc70c5f
Abdullah Afzal <f2024408154@umt.edu.pk>
2026-06-12 17:10:49 +0500

    Updated log parser

--- parser/log_parser.py
+++ parser/log_parser.py
@@ -7,3 +7,8 @@ def parse_log(file_path):
     except Exception as e:
         logs.append(f"Error reading file: {e}")
     return logs
+if __name__ == "__main__":
+    logs = parse_log("parser/sample_logs/auth.log")
+    for log in logs:
+        print(log)
+
